9c81816dc6c33f2e5d38f64f2819ff58303c487c
gusttavvo <gusta.gustavocruz@gmail.com>
2026-06-23 18:56:21 -0300

    first commit

---
+++ src/main/java/dev/gustavvocruz/nba_zone/NbaZoneApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavvocruz.football_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gustavvocruz/football_zone/NbaZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavvocruz.football_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
